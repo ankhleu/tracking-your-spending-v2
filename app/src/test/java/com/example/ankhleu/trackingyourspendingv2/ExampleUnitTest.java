f4fe605db6002c8109f223680fcb3ade27a481b9
Ankhleu <summerlightwind@gmail.com>
2018-01-20 22:50:39 +0800

    測試新增資料,用日期來比較新增資料

--- app/src/test/java/com/example/ankhleu/trackingyourspendingv2/ExampleUnitTest.java
+++ app/src/test/java/com/example/ankhleu/trackingyourspendingv2/ExampleUnitTest.java
@@ -13,18 +13,23 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
+
+/*    @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+*/
 
     @Test
    public void test_add_data()throws Exception
     {
         accountpayDAO dao=new accountpayDAO();
         dao.add(new account(20180120,300,"餐飲","",""));
-        dao.add(new account(20180120,100,"飲料","",","));
-        assertEquals(20180120,20180120,dao.getList().size());
+        dao.add(new account(20180120,100,"飲料","",""));
+        dao.add(new account(20180119,500,"餐飲","",""));
+        //assertEquals(2,20180120,dao.getList().size());用資料筆數比不行
+        assertEquals(20180119,20180120,dao.getList().size());
+        //用日期比較可以
 
 
     }
