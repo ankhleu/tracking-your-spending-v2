e65d23150893e915815059564208afdf3279b112
Ankhleu <summerlightwind@gmail.com>
2018-01-20 20:08:28 +0800

    修改宣告變數,增加建構式

--- app/src/main/java/com/example/ankhleu/trackingyourspendingv2/account.java
+++ app/src/main/java/com/example/ankhleu/trackingyourspendingv2/account.java
@@ -12,17 +12,29 @@ public class account {
     String year;    //年,從日曆開始
     String month;   //月,從日曆開始
     String day;     //日,從日曆開始
-    int Budget;     //預算,用於有活動？
+ //   int Budget;     //預算,用於有活動？
 
     ArrayList<ArrayList<String>> pay=new ArrayList<ArrayList<String>>();  //pay為~有活動~的付錢金額
     ArrayList<ArrayList<String>> pay2=new ArrayList<ArrayList<String>>();  //pay2為~沒有~活動項目~的付錢金額
 
     String kind;    //類別，為輸入金額的項目分類
 
-    String myself;  //沒活動類別
-    String travel;  //有活動類別
+//    String myself;  //沒活動類別
+//   String travel;  //有活動類別
 
     String receipt; //收據，為輸入金額，下方給商品拍照的圖檔
     String notes;   //備註，為輸入金額，下方給商品輸入註解的文字
+
+
+    public account(String year,String month,String day,String kind,String receipt,String notes)
+    {
+        this.year=year;
+        this.month=month;
+        this.day=day;
+        this.kind=kind;
+        this.receipt=receipt;
+        this.notes=notes;
+    }
+
 
 }
